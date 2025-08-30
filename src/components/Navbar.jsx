7cb61a14988ca563ffc933550a2e8320eb390cbe
Roko Falak <rofa.marketing@gmail.com>
2025-08-30 19:12:50 +0200

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,6 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-
             <span className='sm:block hidden'> | FalakAI</span>
           </p>
         </Link>
