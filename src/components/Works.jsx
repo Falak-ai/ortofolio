94ad5bd99e07fc419ef77df6c9486af1b143b680
Roko Falak <rofa.marketing@gmail.com>
2025-08-31 17:14:41 +0200

    Updated Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -53,7 +53,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full min-h-[400px] flex flex-col"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
           <img
@@ -78,20 +78,20 @@ const ProjectCard = ({
           )}
         </div>
 
-        <div className="mt-5 flex-1 flex flex-col">
+        <div className="mt-5">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px] flex-1">{description}</p>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
 
-          <div className="mt-4 flex flex-wrap gap-2">
-            {tags.map((tag) => (
-              <p
-                key={`${name}-${tag.name}`}
-                className={`text-[14px] ${tag.color}`}
-              >
-                #{tag.name}
-              </p>
-            ))}
-          </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
         </div>
       </Tilt>
     </div>
