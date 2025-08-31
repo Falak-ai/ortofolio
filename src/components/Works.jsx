8ea35169ad7f2d5bb703e0cfb6a3b1d9f18d70fb
Roko Falak <rofa.marketing@gmail.com>
2025-08-31 17:12:57 +0200

    Make all project cards the same height

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -53,7 +53,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full min-h-[400px] flex flex-col"
       >
         <div className="relative w-full h-[230px]">
           <img
@@ -78,20 +78,20 @@ const ProjectCard = ({
           )}
         </div>
 
-        <div className="mt-5">
+        <div className="mt-5 flex-1 flex flex-col">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
+          <p className="mt-2 text-secondary text-[14px] flex-1">{description}</p>
 
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p
+                key={`${name}-${tag.name}`}
+                className={`text-[14px] ${tag.color}`}
+              >
+                #{tag.name}
+              </p>
+            ))}
+          </div>
         </div>
       </Tilt>
     </div>
