80163c91102e0eba11a2ed38049786519f171f8b
Roko Falak <rofa.marketing@gmail.com>
2025-08-31 17:45:42 +0200

    Updated Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -109,7 +109,7 @@ const Contact = () => {
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
+              placeholder='Tell me briefly what you need help with (e.g., website design, automation, strategy)'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
